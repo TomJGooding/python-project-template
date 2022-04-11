52606d122a5c557006762449284514a4d4f87b64
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2022-04-11 23:54:42 +0100

    Update test_project_name.py

--- tests/test_project_name.py
+++ tests/test_project_name.py
@@ -2,13 +2,13 @@ import project_name
 
 
 def test_add():
-    assert main.add(3, 4) == 7
-    assert main.add(3.5, 4) == 7
-    assert main.add(3.9, 4) == 7
-    assert main.add(3.9, 4.1) == 8
+    assert project_name.add(3, 4) == 7
+    assert project_name.add(3.5, 4) == 7
+    assert project_name.add(3.9, 4) == 7
+    assert project_name.add(3.9, 4.1) == 8
 
 
 def test_to_sentence():
-    assert main.to_sentence('apple') == 'Apple.'
-    assert main.to_sentence('Apple trees') == 'Apple trees.'
-    assert main.to_sentence('Apple trees.') == 'Apple trees.'
+    assert project_name.to_sentence('apple') == 'Apple.'
+    assert project_name.to_sentence('Apple trees') == 'Apple trees.'
+    assert project_name.to_sentence('Apple trees.') == 'Apple trees.'
